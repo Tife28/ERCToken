e0984fd58797758fad76a6b0103993ace430dd0d
Tife <61167252+Tife28@users.noreply.github.com>
2022-05-17 11:34:23 +0100

    Update ERC20_createtoken.sol

--- ERC20_createtoken.sol
+++ ERC20_createtoken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-
+//import openzeppelin library
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-//Create token name-Token using symbol-Naira
+//Create token name-Token using symbol-Pounds
 contract Token is ERC20 {
     constructor(uint256 initialSupply) ERC20("Token", "Pounds") {
         _mint(msg.sender, initialSupply);
