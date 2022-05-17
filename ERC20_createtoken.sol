39f891ff81773aab0350446268591c2762753e5f
Tife <61167252+Tife28@users.noreply.github.com>
2022-05-17 06:19:47 +0100

    Update ERC20_createtoken.sol

--- ERC20_createtoken.sol
+++ ERC20_createtoken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.3;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/constracts/token/ERC20/ERC20.sol";
 
+//Create token name-Token using symbol-Naira
 contract Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Token", "SYM") {
+    constructor(uint256 initialSupply) ERC20("Token", "Naira") {
         _mint(msg.sender, initialSupply);
     }
 }
