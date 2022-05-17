c15554cc93099bda322cc3301d69a326e4c32101
Tife <61167252+Tife28@users.noreply.github.com>
2022-05-17 05:50:47 +0100

    Update ERC20_createtoken.sol

--- ERC20_createtoken.sol
+++ ERC20_createtoken.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.3;
 
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/constracts/token/ERC20/ERC20.sol"
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/constracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
     constructor(uint256 initialSupply) ERC20("Token", "SYM") {
